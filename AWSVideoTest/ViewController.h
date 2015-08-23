ee595cf4516c8f35a99de19f08ce21c075daef9f
madly <madly@fbd.com>
2015-08-23 12:06:18 +0300

    - initial commit

---
+++ AWSVideoTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AWSVideoTest
+//
+//  Created by Mohamed Adly on 8/23/15.
+//  Copyright (c) 2015 Mohamed Adly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
